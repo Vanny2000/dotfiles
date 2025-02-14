db3a3209f17c0a98afe02df3ccad46c88da1f833
Vanny <kongvanny2000@gmail.com>
2025-02-14 11:23:56 +0700

    update alpha dashboard and copilot-chat username

--- .config/nvim/lua/thuan/plugins/copilot-chat.lua
+++ .config/nvim/lua/thuan/plugins/copilot-chat.lua
@@ -1,4 +1,5 @@
-local full_name = os.getenv("USER") or "Unknown"
+local user = vim.env.USER or "User"
+user = user:sub(1, 1):upper() .. user:sub(2)
 
 return {
 	{
@@ -9,13 +10,15 @@ return {
 		},
 		build = "make tiktoken", -- Only on MacOS or Linux
 		opts = {
-			question_header = " " .. full_name,
+			auto_insert_mode = true,
+			question_header = "  " .. user .. " ",
 			answer_header = " ",
 			error_header = "🚨",
 			vim.keymap.set({ "n", "v" }, "<leader>aa", "<cmd>CopilotChat<CR>", {
 				desc = "Open Copilot Chat",
 			}),
 		},
+
 		-- See Commands section for default commands if you want to lazy load on them
 	},
 }
