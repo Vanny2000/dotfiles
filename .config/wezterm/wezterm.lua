ac7a37151708f589ebeb5391301ee4d394716fe3
Vanny <kongvanny2000@gmail.com>
2025-02-19 16:02:26 +0700

    wezterm refac

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -25,8 +25,8 @@ config.enable_tab_bar = false
 config.window_padding = {
 	left = 0,
 	right = 0,
-	top = 5,
-	bottom = 5,
+	top = 0,
+	bottom = 0,
 }
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
