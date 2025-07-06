5dc63ec58e4c51708c3313c8d6c699d324b6b03b
Vanny <kongvanny2000@gmail.com>
2025-07-07 00:46:39 +0700

    update

---
+++ .config/nvim/lua/thuan/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	---@module 'render-markdown'
+	---@diagnostic disable-next-line: undefined-doc-name
+	---@type render.md.UserConfig
+	opts = {
+		file_types = { "copilot-chat" },
+	},
+}
