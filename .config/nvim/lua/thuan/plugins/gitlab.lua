0ca8cf2816816c31f03fdb023d759b6431faf843
Vanny <kongvanny2000@gmail.com>
2025-07-15 12:00:00 +0700

    Auto-update: 2025-07-15 12:00:00

--- .config/nvim/lua/thuan/plugins/gitlab.lua
+++ .config/nvim/lua/thuan/plugins/gitlab.lua
@@ -5,7 +5,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		"sindrets/diffview.nvim",
 		"stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
-		"nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
+		"echasnovski/mini.icons", -- Recommended but not required. Icons in discussion tree.
 	},
 	build = function()
 		require("gitlab.server").build(true)
--- .config/nvim/lua/thuan/plugins/lualine.lua
+++ .config/nvim/lua/thuan/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "echasnovski/mini.icons" },
 	config = function()
 		-- Gruvbox Dark Hard colors
 		local colors = {
