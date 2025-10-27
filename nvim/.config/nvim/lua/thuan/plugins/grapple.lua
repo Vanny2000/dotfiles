fb021727f0301d63ed65a7242221d65beb8911cb
Vanny <kongvanny2000@gmail.com>
2025-10-27 09:45:05 +0700

    grapple config

--- nvim/.config/nvim/lua/thuan/plugins/grapple.lua
+++ nvim/.config/nvim/lua/thuan/plugins/grapple.lua
@@ -14,7 +14,11 @@ return {
 		{ "<leader>2", "<cmd>Grapple select index=2<cr>", desc = "Select second tag" },
 		{ "<leader>3", "<cmd>Grapple select index=3<cr>", desc = "Select third tag" },
 		{ "<leader>4", "<cmd>Grapple select index=4<cr>", desc = "Select fourth tag" },
-		{ "<leader>5", "<cmd>Grapple select index=4<cr>", desc = "Select fifth tag" },
+		{ "<leader>5", "<cmd>Grapple select index=5<cr>", desc = "Select fifth tag" },
+		{ "<leader>6", "<cmd>Grapple select index=6<cr>", desc = "Select sixth tag" },
+		{ "<leader>7", "<cmd>Grapple select index=7<cr>", desc = "Select seventh tag" },
+		{ "<leader>8", "<cmd>Grapple select index=8<cr>", desc = "Select eighth tag" },
+		{ "<leader>9", "<cmd>Grapple select index=9<cr>", desc = "Select ninth tag" },
 
 		{ "<c-s-n>", "<cmd>Grapple cycle_tags next<cr>", desc = "Go to next tag" },
 		{ "<c-s-p>", "<cmd>Grapple cycle_tags prev<cr>", desc = "Go to previous tag" },
