e64f074470ce570db27448dde9a4553ef518159a
Vanny <kongvanny2000@gmail.com>
2025-12-26 11:14:32 +0700

    refactor/new-file-php

--- nvim/.config/nvim/lua/thuan/plugins/newfile.lua
+++ nvim/.config/nvim/lua/thuan/plugins/newfile.lua
@@ -1,12 +1,16 @@
 return {
-	"adibhanna/nvim-newfile.nvim",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("nvim-newfile").setup({
-			-- Optional configuration
-		})
-		vim.keymap.set("n", "<leader>nf", ":NewFileHere<CR>", { desc = "Create new file here" })
-	end,
-}
+		"adibhanna/nvim-newfile.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("nvim-newfile").setup({
+				templates = {
+					php = {
+						class = "<?php\n\nnamespace %s;\n\nfinal class %s\n{\n\t// TODO: Implement class\n}\n",
+					},
+				},
+			})
+			vim.keymap.set("n", "<leader>nf", ":NewFileHere<CR>", { desc = "Create new file here" })
+		end,
+	}
