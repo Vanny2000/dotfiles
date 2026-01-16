134df7a680f1646e95a457c2f4e788fbaea43dd8
Vanny <kongvanny2000@gmail.com>
2026-01-16 13:55:24 +0700

    refactor(markdown): update key mappings for Obsidian commands
    - Change commands to use lowercase in key mappings
    - Improve consistency in command naming conventions

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
 local keymap = vim.keymap
-keymap.set("n", "<leader>od", "<cmd>ObsidianDailies<CR>", { desc = "Open/Create daily notes" })
-keymap.set("n", "<leader>ot", "<cmd>ObsidianNewFromTemplate<CR>", { desc = "Create new note from template" })
-keymap.set("v", "<leader>oe", "<cmd>ObsidianExtractNote<CR>", { desc = "Extact note" })
-keymap.set("n", "<leader>ow", "<cmd>ObsidianWorkspace<CR>", { desc = "Switch Workspaces" })
-keymap.set("n", "<leader>op", "<cmd>ObsidianPasteImg<CR>", { desc = "Paste image" })
-keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "New note" })
+keymap.set("n", "<leader>od", "<cmd>Obsidian dailies<CR>", { desc = "Open/Create daily notes" })
+keymap.set("n", "<leader>ot", "<cmd>Obsidian new_from_template<CR>", { desc = "Create new note from template" })
+keymap.set("v", "<leader>oe", "<cmd>Obsidian extract_note<CR>", { desc = "Extact note" })
+keymap.set("n", "<leader>ow", "<cmd>Obsidian  workspace<CR>", { desc = "Switch Workspaces" })
+keymap.set("n", "<leader>op", "<cmd>Obsidian paste_img<CR>", { desc = "Paste image" })
+keymap.set("n", "<leader>on", "<cmd>Obsidian new<CR>", { desc = "New note" })
