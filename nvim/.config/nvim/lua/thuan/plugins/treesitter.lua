80ae67eaf0bd35c6709ab8cabdf40697a0517377
Vincent <vanny@shoprunback.com>
2026-04-08 16:20:14 +0700

    chore(nvim0.12): treesitter nvim 0.12 compatible

--- nvim/.config/nvim/lua/thuan/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/thuan/plugins/treesitter.lua
@@ -1,64 +1,102 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-		"nvim-treesitter/nvim-treesitter-textobjects",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			local ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"vimdoc",
+				"c",
+				"php",
+				"python",
+				"http",
+				"vue",
+				"go",
+				"blade",
+			}
+
+			-- Only install parsers that are missing (avoids reinstall on every startup)
+			local already_installed = require("nvim-treesitter.config").get_installed("parsers")
+			local to_install = vim.iter(ensure_installed)
+				:filter(function(p)
+					return not vim.tbl_contains(already_installed, p)
+				end)
+				:totable()
+			if #to_install > 0 then
+				require("nvim-treesitter").install(to_install)
+			end
+
+			-- Filetypes that should get treesitter highlight / fold / indent.
+			-- These map to the parsers above (note: tsx -> typescriptreact, vimdoc -> help, etc.)
+			local filetypes = {
+				"json",
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"svelte",
+				"graphql",
+				"bash",
+				"sh",
+				"zsh",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"help",
+				"c",
+				"php",
+				"python",
+				"http",
+				"vue",
+				"go",
+				"blade",
+			}
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = filetypes,
+				callback = function()
+					-- syntax highlighting (Neovim core)
+					pcall(vim.treesitter.start)
+					-- folds (Neovim core)
+					vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.wo.foldmethod = "expr"
+					-- indentation (nvim-treesitter)
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
 	},
-	config = function()
-		-- import nvim-treesitter plugin
-		local treesitter = require("nvim-treesitter.configs")
-		-- configure treesitter
-		---@diagnostic disable-next-line: missing-fields
-		treesitter.setup({ -- enable syntax highlighting
-			highlight = {
-				enable = true,
-			},
-			-- enable indentation
-			indent = { enable = true },
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enable = true,
-			},
-		-- ensure these language parsers are installed
-		ensure_installed = {
-			"json",
-			"javascript",
-			"typescript",
-			"tsx",
-			"yaml",
-			"html",
-			"css",
-			"prisma",
-			"markdown",
-			"markdown_inline",
-			"svelte",
-			"graphql",
-			"bash",
-			"lua",
-			"vim",
-			"dockerfile",
-			"gitignore",
-			"query",
-			"vimdoc",
-			"c",
-			"php",
-			"python",
-			"http",
-			"vue",
-			"go",
-			"blade",
-		},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<leader>v",
-					node_incremental = "<TAB>",
-					scope_incremental = false,
-					node_decremental = "<S-TAB>",
-				},
-			},
-		})
-	end,
+	{
+		"windwp/nvim-ts-autotag",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
 }
