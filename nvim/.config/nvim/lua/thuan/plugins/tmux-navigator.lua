221ac805b67b208524ad6746ef14301ac3d9e1a6
Vanny <kongvanny2000@gmail.com>
2026-06-24 13:53:36 +0700

    .

--- nvim/.config/nvim/lua/thuan/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/thuan/plugins/tmux-navigator.lua
@@ -1,5 +1,13 @@
 return {
 	"christoomey/vim-tmux-navigator",
+	-- Don't let the plugin create its own mappings. Its built-in terminal-mode
+	-- mappings are written for Vim (they use <C-w> to leave terminal mode, which
+	-- does nothing in Neovim) so they leak "TmuxNavigateDown" into the shell, and
+	-- they clobber the floating-terminal <C-j>/<C-k> maps in plugin/floaterm.lua.
+	-- The normal-mode nav we want is defined via `keys` below.
+	init = function()
+		vim.g.tmux_navigator_no_mappings = 1
+	end,
 	cmd = {
 		"TmuxNavigateLeft",
 		"TmuxNavigateDown",
@@ -9,10 +17,10 @@ return {
 		"TmuxNavigatorProcessList",
 	},
 	keys = {
-		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		{ "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
+		{ "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
+		{ "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
+		{ "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+		{ "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
 	},
 }
